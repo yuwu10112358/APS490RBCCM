ad2ed6c4197f01fad2dc597c2399bcb988a942d6
hojewel <jewelho@inside-65-27-226.wireless.utoronto.ca>
2015-11-09 15:58:00 -0500

    Jewel added

--- capstone_script.r
+++ capstone_script.r
@@ -1,4 +1,4 @@
-
+### TEST###
 
 #backtester <- function(stock_price,market_price,amount_invested,startdate,enddate){
 #} 
@@ -14,7 +14,6 @@ output
 
 # buy = 1; hold = 0; sell = -1
 strategy <- function(stock_price,market_price,amount_invested,startdate,enddate){
-  
   for(i in 1:length(stock_price[1,])){
     if(stock_price[2,i] >= 110){
       snow = c(stock_price[1,i],stock_price[2,i],-1)
@@ -26,8 +25,6 @@ strategy <- function(stock_price,market_price,amount_invested,startdate,enddate)
    # elseif(stock_price[2,i] = 105)
     #    if(snow[3] == -1) 
      # snow = c(stock_price[1,i],stock_price[2,i],0)
-    
-        
     }
 }
 
