65e4c0667625223d0c5ad49730c735bcb745486d
Gordon Dri <gordon.dri@gmail.com>
2016-02-07 19:51:24 -0500

    made changes to IPR strategy, missing the part which sends order to market

--- function file.R
+++ function file.R
@@ -1,24 +1,7 @@
-stocklist <- c("AC", "BNS", "BMO")
-namelist <- c("tick")
-
 # constants
 jump <- 13
 buy_IPR <- 0.6
 sell_IPR <- 0.2
-
-
-# first day of trading 
-start_trade <- which(tick_data$Date == "2015-06-08 09:30:00")
-# last day of trading
-end_trade <- which(tick_data$Date == "2015-11-24 15:59:00")
-
-for (stock in stocklist){
-  # assign each df to a variable for later use 
-  for (nme in namelist){
-    stock_data <- paste(stock,nme,sep="_")
-    assign(paste(stock,nme,sep="-"), global_tables[[stock_data]])
-  }
-}
 
 return_and_stdev <- function(prices){
   for (i in 2:NROW(prices)){
@@ -33,15 +16,25 @@ return_and_stdev <- function(prices){
 }
 
 IPR_df <- data.frame(Date = as.character(), Symbol = as.character(), IPR = as.integer())
+Stocks <- c("AC", "BNS", "BMO")
+EquityList <- c("tick", "ask", "bid")
+a <- 1
+# only trading every 30 minutes, change this value when trading time is different 
+end_a <- nrow(list_dates)*12*30
 
-# loop through each minute (a) 
-for (a in start_date:end_date){
-  for (stock in stocklist){
-    stock_data <- paste(stock,namelist[1],sep="_")
+# loop through each minute (a) but only calculate IPR every 30 minutes  
+while (a < end_a){
+  print(a)
+  for (stock in Stocks){
+    stock_data <- paste(stock,EquityList[1],sep="_")
     tick_data <- env[[stock_data]]
-    next_date <- tick_data[a, "Date"] + 1740
-    P_asterix <- tick_data[which(tick_data$Date == next_date), "LAST_PRICE"]
-    P_asterix_j_date <-  tick_data[which(tick_data$Date == next_date) - jump, "LAST_PRICE"]
+    if (strftime(tick_data[a, "Date"], format="%H:%M:%S") == "15:30:00"){
+      a <- a + 1
+      break
+    }
+    next_date <- tick_data[a+29, "Date"] 
+    P_asterix <- tick_data[a+29, "LAST_PRICE"]
+    P_asterix_j_date <-  tick_data[(a+29) - jump, "LAST_PRICE"]
     start_row <- 1
     end_row <- which(tick_data$Date == next_date)
     price_estimates <- data.frame(Price = tick_data[start_row:end_row, "LAST_PRICE"])
@@ -49,31 +42,26 @@ for (a in start_date:end_date){
     stdev <- return_and_stdev(price_estimates)$stdev
     z <- (log(P_asterix/P_asterix_j_date) - jump * ret) / (sqrt(jump * stdev))
     IPR <- pnorm(z)
-    IPR_df <- rbind(IPR_df, data.frame(Date = next_date + 60, Symbol = stock, IPR = z))
-    if (stock == stock_list[length(stock_list)]){
-      temp_dframe <- subset(IPR_df, Date == next_date)
+    IPR_df <- rbind(IPR_df, data.frame(Date = next_date + 60, Symbol = stock, IPR = IPR))
+    if (stock == Stocks[length(Stocks)]){
+      temp_dframe <- subset(IPR_df, Date == "2015-05-13 10:00:00")
       # order the dataframe based on IPR values 
       temp_dframe <- temp_dframe[order(temp_dframe$IPR),]
-      # assign percentages to each stock for cash allocations (HARD-CODED)
-      temp_dframe[1, "Pct"] <- 0.5
-      temp_dframe[2, "Pct"] <- 0.3
-      temp_dframe[3, "Pct"] <- 0.2
       # create buy or sell orders
       for (b in length(temp_dframe)){
-        if (temp_dframe[b, "IPR"] >= buy_IPR){
-          # send an order to the market with buy and using the existing cash * appropriate Pct
-        } else if (temp_dframe[b, "IPR"] <= sell_IPR){
+        if (temp_dframe[b, "IPR"] >= 0.5){
+          # assign percentages to each stock for cash allocations
+          cash_alloc <- -(temp_dframe[b, "IPR"]) + 0.5
           # send an order to the market with sell and using the existing cash * appropriate Pct
+        } else if (temp_dframe[b, "IPR"] < 0.5){
+          # assign percentages to each stock for cash allocations
+          cash_alloc <- -(temp_dframe[b, "IPR"]) + 0.5
+          # send an order to the market with buy and using the existing cash * appropriate Pct
         }
       }
     }
   }
-  a <- a + 29
+  a <- a + 30
 }
-
-
-  
-  
-
 
 
