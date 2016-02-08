05cf06371f46766f682d008567b3cd10e80cbd5e
hojewel <jewelho@inside-65-20-37.wireless.utoronto.ca>
2016-02-08 18:23:32 -0500

    trying to test

--- backtest_lib.r
+++ backtest_lib.r
@@ -1,12 +1,19 @@
 source('constants.r')
+source('data_cleaning_jewel.r')
 library( "RWeka" )
 library(XLConnect)
+
 
  #untested
 update_pendingorderbook <- function (env, timestamp, symbol){
   #orderbook is a referene (pointer in an environment), and changes are meant to be permanent
   #taking in orderbook as argument and returns a list containing execution messages
   #for the purpose of this back testing order book will only contain pending limit orders
+ 
+  #env <-global_tables
+  #symbol <- "BNS"
+  #timestamp <- "2015-05-13 09:41:00 EDT"
+  
   orderbook <- env[[Con_GlobalVarName_LOB]]
   quotes <- getquotes(env, symbol, timestamp)
   
